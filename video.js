ddb83ee681269202bd6f409bd9f58a9d61513069
vitor1209 <vitor.silva.lopes1209@gmail.com>
2024-12-03 16:01:54 -0300

    camera

--- video.js
+++ video.js
@@ -1,10 +1,14 @@
 function startVideoFromCamera() {
-    navigator.mediaDevices.getUserMedia({ video: true })
+    const specs = { video: { width: 1080, aspectRatio: 21 / 9 } };
+
+    navigator.mediaDevices.getUserMedia(specs)
         .then(stream => {
-            const videoElement = document.querySelector("#video");
+            const videoElement = document.querySelector('#video');
             videoElement.srcObject = stream;
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 window.addEventListener("DOMContentLoaded", startVideoFromCamera);
